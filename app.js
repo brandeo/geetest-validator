89aca292be90f3ad2681722e926a0e64243be148
ikenxuan <2066855608@qq.com>
2023-12-15 07:37:25 +0800

    fix

--- app.js
+++ app.js
@@ -162,11 +162,15 @@ server.post("/geetest", async (request, reply) => {
 
   if (user && gt && challenge) {
     console.log(`Token verified for user: ${user}`);
+    const validate = await get_validate(gt, challenge)
     const resultdata = {
       status: 0,
       message: "OK",
       data: {
-        validate: await get_validate(gt, challenge),
+        geetest_gt: gt,
+        geetest_challenge: challenge,
+        geetest_validate: validate,
+        geetest_seccode: validate + '|jordan'
       },
     };
     await reply.type("application/json").send(resultdata);
